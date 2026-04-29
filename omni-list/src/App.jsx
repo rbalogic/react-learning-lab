512b7e9ba7c77b0fed3bfaf89979a0a59f48f266
Balaji R <rbalajives@gmail.com>
2026-04-29 06:49:13 +0530

    task: adds base layouts

---
+++ omni-list/src/App.jsx
@@ -0,0 +1,13 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Omni List</h1>
+      <input type="text"></input>
+      <button>Save</button>
+      <h3>Items</h3>
+      <p>To be added.</p>
+    </div>
+  );
+};
+
+export default App;
