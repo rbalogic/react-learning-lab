555942355a457ae9a2f4a7ac3cfb676d9e7fbf50
Balaji R <rbalajives@gmail.com>
2026-05-09 20:39:25 +0530

    feat: adds question shuffle

--- quiz-show/src/components/Quiz.jsx
+++ quiz-show/src/components/Quiz.jsx
@@ -1,22 +1,37 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import completedLogo from "../assets/quiz-complete.png";
 
 const Quiz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
 
+  const isQuizComplete = activeQuestionIndex === QUESTIONS.length;
+
+  if (isQuizComplete) {
+    return (
+      <div id="summary">
+        <img src={completedLogo} />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   const handleAnswerSelect = (selectedAnswer) => {
     setUserAnswers((prevUserAnswer) => {
       return [...prevUserAnswer, selectedAnswer];
     });
-    console.log(userAnswers);
   };
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li className="answer" key={answer}>
               <button onClick={() => handleAnswerSelect(answer)}>
                 {answer}
